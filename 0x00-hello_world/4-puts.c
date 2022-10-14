26e3ebb5218bec8c3f83ad832d72973021d356c0
Starra777 <daniellaochade17@gmail.com>
2022-10-13 18:00:29 -0700

    puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,36 @@
 #!/bin/bash
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Prints "Programming is like building a multilingual
- *
- *   *                puzzle, followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * main - Prints "Programming is like building a multilingual puzzle"
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
+	return (0);
 }
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
